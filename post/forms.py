fab9292169e5a49eaeed92d21f9e5c75b730b343
SEONGIK KIM <kpic1638@gmail.com>
2020-02-05 15:00:29 +0900

    Fix tagname expression

--- post/forms.py
+++ post/forms.py
@@ -5,5 +5,5 @@ from .models import Post
 class PostModelForm(ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = '__all__' #여기서 user는 request.user로 자동지정해주고 필드에서 빼야함
 
--- post/views.py
+++ post/views.py
@@ -8,6 +8,16 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all() # 실제로는 내가 속한 그룹 인원들의 게시물만 가져와야함.
     #posts = Post.objects.filter(tags__name__in=["찾을태그"])
+    '''
+    만약 ["찾을태그1","찾을태그2"]식으로 여러개의 태그를 넣어 검색할 경우
+    태그 1과 태그 2를 동시에 가지고 있는 게시물은 두 번 조회될 수 있다(쿼리셋에 2번이나 들어오게 된다)
+    이때는 쿼리문 제일 끝에 .distinct()를 붙여주면 포스트의 중복을 막을 수 있다.
+    '''
+    #qs = str(Post.tags.all()) 이렇게 할 시 포스트 모델에 존재하는 모든 태그를 가져올 수 있다.
+
+    #만약 태그를 눌러 a태그링크로 검색했다면....
+
+
     context = {
         'posts': posts,
     }
@@ -24,7 +34,6 @@ def post_create(request):
             #포스트모델폼의 정보가 유효하면, post에 할당한 뒤 FK를 접속한 유저로 지정하여 저장한다.
             post = postform.save(commit=False)
             post.user = request.user
-            #post.photo = request.FILES['photo'] 굳이 필요없는 코드?
             post.save()
             return redirect('post:post_list')
     else:
@@ -36,6 +45,8 @@ def post_create(request):
 
 def post_detail(request,pk):
     post = Post.objects.get(id=pk)
+
+    #qs = ''.join(post.tags.)
     context = {
         'post': post,
     }
