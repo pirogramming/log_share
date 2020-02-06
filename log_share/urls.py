8cf52db8e92070bf549ed129b98e2805dc2c5f29
briggirl2 <briggirl2@gmail.com>
2020-02-06 16:15:39 +0900

    before merge

--- log_share/urls.py
+++ log_share/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('group_management.urls'))
+    path('group/', include('group_management.urls'))
 ]
