d04c0c2bfd946467a5e431df92c52737462a9ae7
cmoweg <cmoweg@gmail.com>
2020-02-07 14:15:17 +0900

    For me

--- log_share/settings.py
+++ log_share/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'log_share.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'log_share',
+        'USER': 'root',
+        'PASSWORD': '111111',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
