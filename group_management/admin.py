747832d8ff8cce94f6032c584705f0209c8b2407
briggirl2 <briggirl2@gmail.com>
2020-02-11 15:02:18 +0900

    group change -ing...

--- group_management/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from group_management.models import CustomGroup, GroupRequest
-
-admin.site.register(CustomGroup)
-admin.site.register(GroupRequest)
--- group_management/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GroupManagemnetConfig(AppConfig):
-    name = 'group_management'
