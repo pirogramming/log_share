1ecee7e9488b8b1a440d04544a7c05e8a745b90c
BunnyBin <dbqls8428@gmail.com>
2020-02-13 00:15:37 +0900

    활동보기/피드보기 - 무한스크롤 완성

--- myprofile/views.py
+++ myprofile/views.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, render_to_response
 from django.contrib.auth.models import User
-from django.template import RequestContext
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from accounts.forms import SignupModelForm, CustomUserChangeForm
 from myprofile.models import BookMark, Profile
-from group_management.models import CustomGroup
 
 # <<<<<<<<<<<<<<profile user_pk>>>>>>>>>>>>>>
+from post.models import Post
+
+
 @login_required
 def profile_detail(request, pk):
     # 본인 or 관련 그룹원
@@ -30,20 +31,31 @@ def profile_detail(request, pk):
 
     else:
         profile = user.user_profile  # user -> profile
-        posts = user.user_post.all()  # user -> post
 
         # 최근 태그 리스트
-        # 시작일 기준(만약 같다면 종료일 기준)으로 최근 게시물부터 태그가 들어가게 한다.
-        recent_posts = posts.order_by('-start_date', '-end_date')
+        post_list = user.user_post.order_by('-start_date', '-end_date')  # 시작일 기준(만약 같다면 종료일 기준)으로 최근 게시물부터 태그가 들어가게 한다.
         # todo 최근 게시물의 개수제한 필요 (최근 5개 게시물이라던가)
         # 아래 딕셔너리는 orderdDict여야 하므로 파이썬 3.6 버전 이상에서만 제대로 동작하는 코드.
         recent_tags_count = {}
-        for post in recent_posts:
+        for post in post_list:
             for tag in post.tags.all():
                 if tag in recent_tags_count:
                     recent_tags_count[tag] += 1
                 else:
                     recent_tags_count[tag] = 1
+
+        # myprofile:profile_detail과 post:myprofile_post_list가 연동된 상태
+        # myprofile:profile_detail 내 post:myprofile_post_list가 처음 화면
+        # 스크롤을 내리면 post:myprofile_post_list_ajax 실행
+        paginator = Paginator(post_list, 2)  # 한페이지에 담길 포스트 갯수
+        page = request.GET.get('page')  # 첫 화면의 페이지(GET)
+
+        try:
+            posts = paginator.page(page)  # page(): 몇번째 페이지 리턴
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)  # num_pages : 총 페이지 수
 
         context = {
             'profile': profile,
