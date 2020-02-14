70cb4690686188e671da9497c4d6aa3bb394e93a
cmoweg <cmoweg@gmail.com>
2020-02-14 17:57:24 +0900

    making filter

--- search/views.py
+++ search/views.py
@@ -61,6 +61,7 @@ def main_search(request):
         # 'results': qs,   # 1: post, 2: user
         'results': results,
         'q': q,
+        'groups': user.user_groups.all()
         # 'option': option,
     })
 
@@ -90,33 +91,18 @@ def tag_search(request, tag_name):
     })
 
 
-# 피드 - 최근 포스팅
-# 그룹 -
 def search_auto(request):
     results = dict()
     user = request.user
     q = request.GET.get('q', '')
     option = request.GET['option']
 
-    # qs = Post.objects.filter(
-    #     Q(user__user_groups__in=user.user_groups.all()) &  # 나와 관련된 유저들
-    #     (Q(title__icontains=q) | Q(contents__icontains=q)) | Q(tags__name__in=q)
-    # ).distinct()  # 중복 제거
-
     if option == 'posts':  # 제목에 q가 포함되어 있는 레코드만 필터링 + 나랑 관련된 사람.
-        # 필터링 종류 #
-        # 그룹 필터링
-        # 기간 필터링(시간, 최신순)
-        # 포스트 카테고리
 
         qs = Post.objects.filter(
             Q(user__user_groups__in=user.user_groups.all()) &  # 나와 관련된 유저들
             (Q(title__icontains=q) | Q(contents__icontains=q) | Q(tags__name__icontains=q))
         ).distinct()  # 중복 제거
-        # qs = relate_post.objects.filter(
-        #     # 포스트 내용 필요한가? 내용 미리보기 필요할듯..(해당 키워드가 담긴 문장을 보여준다던지..)
-        #     Q(title__icontains=q) | Q(tag__word__icontains=q) | Q(contents__icontains=q)
-        # ).distinct()
 
         results = [
             {
@@ -126,8 +112,6 @@ def search_auto(request):
         ]
 
     elif option == 'users':  # 이름에 q 포함 + 접속 유저와 관련된 그룹원
-        # 필터링 종류 #
-        # 그룹 필터링
         qs = User.objects.filter(
             Q(user_groups__in=user.user_groups.all()) &
             (Q(user_profile__name__icontains=q) | Q(username__icontains=q))
@@ -141,9 +125,6 @@ def search_auto(request):
         ]
 
     elif option == 'custom_groups':  # 그룹명에 q가 포함된 그룹
-        # 필터링 종류 #
-        # 그룹 카테고리
-        # if request.user.groups in filtered_group: 해당 그룹원의 최신 포스팅도 같이?
         qs = CustomGroup.objects.filter(
             Q(group_name__icontains=q) & Q(is_searchable=True)
         )
