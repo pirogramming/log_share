ecd841575b3fd550f33874300c7db17c33d9edd9
briggirl2 <briggirl2@gmail.com>
2020-02-14 14:28:24 +0900

    Auto stash before rebase of "origin/suuu"

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
