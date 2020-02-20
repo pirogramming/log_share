92f3d793ccbc7b0cae2880fe2215e8928a32b496
SEONGIK KIM <kpic1638@gmail.com>
2020-02-21 03:53:35 +0900

    Create MyTimeLine by jquery
    힘든싸움이었다...

--- log_share/settings.py
+++ log_share/settings.py
@@ -150,6 +150,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
 # image
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
