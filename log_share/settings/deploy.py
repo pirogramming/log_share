313bb04cb908170729f4b15288ec006cf34e08bc
Ubuntu <ubuntu@ip-172-31-44-99.ap-northeast-2.compute.internal>
2020-02-24 04:09:14 +0000

    배포 보안처리3

--- log_share/settings/deploy.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-
-config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
-
-DEBUG = True
-ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
-
-# WSGI application
-WSGI_APPLICATION = 'log_share.wsgi.deploy.application'
