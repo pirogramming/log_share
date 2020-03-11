9c2ecc217ec7af024bb9bc490aef183f6d65228c
cmoweg <cmoweg@gmail.com>
2020-03-11 16:20:20 +0900

    clean

--- search/utils.py
+++ search/utils.py
@@ -4,7 +4,7 @@ from post.models import Post
 
 
 def filter_posts(q, user):
-    print('utils: posts')
+    # print('utils: posts')
     return Post.objects.filter(
         Q(user__user_groups__in=user.user_groups.all()) &  # 나와 관련된 유저들
         (Q(title__icontains=q) | Q(contents__icontains=q))
@@ -12,7 +12,7 @@ def filter_posts(q, user):
 
 
 def filter_tags(q, user):
-    print('utils: tags')
+    # print('utils: tags')
     return Post.objects.filter(
         Q(user__user_groups__in=user.user_groups.all()) &  # 나와 관련된 유저들
         Q(tags__name__icontains=q)
@@ -30,7 +30,7 @@ def group_id_list(request, user):
     group_list = [id for key, id in request.GET.items() if key[:5] == 'group']
     if not group_list:
         group_list= user.user_groups.all()
-    print(group_list)
+    # print(group_list)
     return group_list
 
 
@@ -39,8 +39,8 @@ def process_category(request, user, qs):
 
     for key, value in request.GET.items():
         rq[key] = request.GET.get(key)
-        print(key, value)
-    print(rq)
+    #     print(key, value)
+    # print(rq)
 
     if 'date-start' not in rq.keys() or rq['date-start'] == '':
         rq['date-start'] = '1900-01-01'
@@ -56,10 +56,10 @@ def process_category(request, user, qs):
             '기타',
         )
 
-    print(rq)
+    # print(rq)
     # print(re)
-    print(qs)
-    print('그룹', group_id_list(request, user))
+    # print(qs)
+    # print('그룹', group_id_list(request, user))
     return qs.filter(
         Q(start_date__gte=rq['date-start']) &
         Q(end_date__lte=rq['date-end']) &
--- search/views.py
+++ search/views.py
@@ -18,21 +18,18 @@ def main_search(request):
     option = request.GET.get('options', '')
     # category = request.GET.get()
     user = request.user
-    print('Request:', request.GET.keys())
+
     qs = None
     # 검색창 옆에 필터 선택 -> request로 받고, 그 값에 따라 결과값 필터링 **
     if q:  # q가 있으면
 
         if option == 'posts':  # 제목에 q가 포함되어 있는 레코드만 필터링 + 나랑 관련된 사람.
             qs = filter_posts(q, user)
-            print('option: posts')
         elif option == 'tags':  # 이름에 q 포함 + 접속 유저와 관련된 그룹원
             qs = filter_tags(q, user)
-            print('option: tags')
 
         elif option == 'users':  # 그룹명에 q가 포함된 그룹
             qs = filter_users(q, user)
-            print('option: users')
 
     else:
         qs = Post.objects.filter(user__user_groups__in=user.user_groups.all()).distinct()
@@ -44,14 +41,9 @@ def main_search(request):
         selected_groupid = groupid
 
     unselected_groupid = groupid - selected_groupid
-    print("카테고리 필터링 전", qs)
     qs = process_category(request, user, qs)
 
-    print("카테고리 필터링 후: ", qs)
     bm_list = user.user_bookmark.values_list('post_id', flat=True)
-    print('북마크 리스트', bm_list)
-    print('그룹 체크박스', list(selected_groupid))
-    print('그룹', unselected_groupid)
     posts = None
     if qs:
 
@@ -64,11 +56,6 @@ def main_search(request):
             posts = paginator.page(1)
         except EmptyPage:
             posts = paginator.page(paginator.num_pages)
-
-    # print(paginator, page)
-    # print(posts)
-    # print('Request.GET:', request.GET)
-    # print(qs)
 
     manage_groups = list(request.user.user_manage_groups.all())
     request_messages = list()
@@ -99,7 +86,6 @@ def tag_search(request, tag_name):
     user = request.user
     q = tag_name
     qs = filter_tags(q, user)
-    print(qs)
     results = {'posts': qs}
     return render(request, 'search/main.html', {
         'request': request,
@@ -114,7 +100,6 @@ def search_auto(request):
     q = request.GET.get('q', '')
     option = request.GET['option']
     results = None
-    print('Ajax요청', q, option)
     if option == 'posts':
         qs = filter_posts(q, user)
         results = [
@@ -124,7 +109,6 @@ def search_auto(request):
             } for post in qs
         ]
     elif option == 'tags':
-        # qs = Post.tags.filter(post__user__user_groups__in=user.user_groups.all())
         qs = Tag.objects.filter(
             Q(post__user__user_groups__in=user.user_groups.all()) &
             Q(name__icontains=q)
@@ -136,8 +120,6 @@ def search_auto(request):
             } for tag in qs
         ]
     elif option == 'users':
-        # qs = User.objects.filter(user_groups__in=user.user_groups.all()).distinct()
-        print(User.objects.filter(user_groups__in=user.user_groups.all()))
         qs = User.objects.filter(
             Q(user_groups__in=user.user_groups.all()) &
             (Q(user_profile__name__icontains=q) | Q(username__icontains=q))
@@ -149,7 +131,6 @@ def search_auto(request):
             } for users in qs
         ]
 
-    print('Ajax응답', results)
 
     data = {
         'results': results,
@@ -165,19 +146,15 @@ def search_scroll(request):
     option = request.GET.get('options', '')
     user = request.user
     qs = None
-    print('스크롤 GET요청', request.GET.values())
     if q:  # q가 있으면
 
         if option == 'posts':  # 제목에 q가 포함되어 있는 레코드만 필터링 + 나랑 관련된 사람.
             qs = filter_posts(q, user)
-            print('option: posts')
         elif option == 'tags':  # 이름에 q 포함 + 접속 유저와 관련된 그룹원
             qs = filter_tags(q, user)
-            print('option: tags')
 
         elif option == 'users':  # 그룹명에 q가 포함된 그룹
             qs = filter_users(q, user)
-            print('option: users')
 
     else:
         qs = Post.objects.filter(user__user_groups__in=user.user_groups.all()).distinct()
@@ -194,11 +171,8 @@ def search_scroll(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    print(page, posts)
-    print('--')
     context = {
         'posts': posts,
         'bm_post_list': bm_list,
-
     }
     return render(request, 'search/main_search.html', context)  # ajax_datatype => dataType: 'html'
